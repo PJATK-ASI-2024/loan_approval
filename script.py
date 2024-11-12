4395e1b5bf9dc9d0e83489467a3a10cb29dc4927
czapkazdaszkiem <jaroslaw.gawrych@gmail.com>
2024-11-12 13:42:31 +0100

    added EDA

--- script.py
+++ script.py
@@ -2,8 +2,12 @@ import logging
 import kagglehub
 import os
 import shutil
+from matplotlib import pyplot as plt
 import pandas as pd
 from sklearn.model_selection import train_test_split
+import seaborn as sns
+import matplotlib.ticker as ticker
+from matplotlib.backends.backend_pdf import PdfPages
 
 logging.basicConfig(
     level=logging.INFO,
@@ -39,5 +43,68 @@ def download_and_split_data():
 
     return df
 
+def EDA(df, pdf_pages):
+
+    print(df)
+    print(df.info())
+
+    print(df.describe().T)
+
+    cat_cols = df.select_dtypes(include=['object', 'category']).columns
+    print(df[cat_cols].describe().T)
+    
+    num_cols = df.select_dtypes(include=['int64', 'float64']).columns
+
+    for col in num_cols:
+
+        plt.figure(figsize=(16, 9))
+
+        plt.subplot(1, 2, 1)
+        plt.hist(df[col].dropna())
+        plt.title(f'{col} Distribution')
+        plt.xlabel(col)
+        plt.ylabel('Count')
+
+        plt.subplot(1, 2, 2)
+        sns.boxplot(x=df[col])
+        plt.title(f'{col} Boxplot')
+        plt.xlabel(col)
+
+        plt.xticks(rotation=45)
+        plt.gca().xaxis.set_major_formatter(ticker.FuncFormatter(lambda x, pos: '{:.2f}'.format(x)))
+
+        pdf_pages.savefig()
+        plt.close()
+    
+    for col in cat_cols:
+        
+        plt.figure(figsize=(16, 9))
+
+        plt.subplot(1, 2, 1)
+        sns.countplot(x=df[col])
+        plt.title(f'{col} Distribution')
+        plt.xlabel(col)
+        plt.ylabel('Count')
+
+        plt.subplot(1, 2, 2)
+        sns.boxplot(x=df[col])
+        plt.title(f'{col} Boxplot')
+        plt.xlabel(col)
+
+        plt.xticks(rotation=45)
+
+        pdf_pages.savefig()
+        plt.close()
+
+    plt.figure(figsize=(12, 7))
+    sns.heatmap(df.drop(cat_cols, axis=1).corr(), annot = True, vmin = -1, vmax = 1)
+    plt.xticks(rotation=45)
+    pdf_pages.savefig()
+    plt.close()
+
+    print(df.isnull().sum())
+
 if __name__ == "__main__":
-    df = download_and_split_data()
+    with PdfPages('plots.pdf') as pdf_pages:
+        df = download_and_split_data()
+        EDA(df, pdf_pages)
